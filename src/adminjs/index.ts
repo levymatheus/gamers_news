60b5f0f7db878d2363a40da2491b805e95cb4459
Levy Matheus <levymatheus2025@gmail.com>
2024-04-17 02:38:02 -0400

    adminJS config with express

---
+++ src/adminjs/index.ts
@@ -0,0 +1,43 @@
+import AdminJS from "adminjs";
+import AdminJSExpress from "@adminjs/express"
+import AdminJSSequelize from "@adminjs/sequelize"
+import { sequelize } from "../database";
+
+AdminJS.registerAdapter(AdminJSSequelize)
+
+export const adminJS = new AdminJS({
+    databases: [sequelize], 
+    rootPath: "/admin", // http://localhost:3000/admin
+    branding: {
+        companyName: 'Gamer News Administrator',
+       logo: '/logoApp.png',
+        softwareBrothers: false,
+        theme: {
+            colors: {
+                primary100: '#02012d',
+                primary80: '#03023f',
+                primary60: '#04034f',
+                primary40: '#050461',
+                primary20: '#060673',
+                grey100: '#151515',
+                grey80: '#333333',
+                grey60: '#4d4d4d',
+                grey40: '#666666',
+                grey20: '#dddddd',
+                filterBg: '#333333',
+                accent: '#0a0a73',
+                hoverBg: '#0c0c85',
+                border: '#0e0e96',
+                highlight: '#1010a8',
+                success: '#28a745',
+                error: '#dc3545',
+                info: '#ffc107',
+                
+    
+            }
+        }
+    }
+
+})
+
+export const adminJsRouter = AdminJSExpress.buildRouter(adminJS) // rotas para o servidor, basicamente comunica o express com o adminjs
